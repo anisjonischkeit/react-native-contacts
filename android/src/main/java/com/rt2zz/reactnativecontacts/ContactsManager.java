63dd99b776f6fc7fb4152362f09835dbd11777dd
Anis Jonischkeit <anis.jonischkeit@gmail.com>
2020-01-21 22:57:33 +0000

    fixed openContactPicker

--- android/src/main/java/com/rt2zz/reactnativecontacts/ContactsManager.java
+++ android/src/main/java/com/rt2zz/reactnativecontacts/ContactsManager.java
@@ -506,6 +506,7 @@ public class ContactsManager extends ReactContextBaseJavaModule implements Activ
         }
     }
 
+    @ReactMethod
     public void openContactPicker(Callback callback) {
         try {
             Uri uri = ContactsContract.Contacts.CONTENT_URI;
